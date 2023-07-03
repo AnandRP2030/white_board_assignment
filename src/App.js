9d92833d99654ec14b30c53ef605e1730cc5ac90
AnandRP2030 <anand.rp2030@gmail.com>
2023-07-03 16:44:01 +0530

    different colors added in the brush

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
-import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useState, useRef } from "react";
 import { fabric } from "fabric";
+import BrushColor from "./Components/BrushColor";
+import "./App.css";
 
 function App() {
+  const [activeBrushColor, setActiveBrushColor] = useState("#000000");
+  const canvasRef = useRef(null);
+
   useEffect(() => {
-    const canvas = new fabric.Canvas("canvas");
+    const canvas = new fabric.Canvas(canvasRef.current);
     canvas.setHeight(window.innerHeight);
     canvas.setWidth(window.innerWidth);
     canvas.isDrawingMode = true;
     canvas.freeDrawingBrush.width = 3;
-    canvas.freeDrawingBrush.color = "#0052cc";
-  }, []);
+    canvas.freeDrawingBrush.color = activeBrushColor;
 
-  return <canvas id="canvas" />;
+    canvas.on("path:created", function (options) {
+      const path = options.path;
+      path.set("stroke", activeBrushColor);
+
+      canvas.requestRenderAll();
+    });
+
+    return () => {
+      canvas.dispose(); 
+    };
+  }, [activeBrushColor]);
+
+  console.log(activeBrushColor);
+  return (
+    <>
+      <BrushColor
+        setActiveBrushColor={setActiveBrushColor}
+        activeBrushColor={activeBrushColor}
+      />
+      <canvas id="canvas" ref={canvasRef} />;
+    </>
+  );
 }
 
 export default App;
